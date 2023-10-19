81365e6c291d452e2efaacb9994363a95746454d
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-19 10:46:10 -0700

    update scripts.js for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,29 @@
-// business logic
-function add(number1, number2) {
-  return number1 + number2;
+// Business Logic
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-// user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-function saySomething(whatToSay) {    
-  window.alert(whatToSay);    
-}   
-
-const result = add(number1, number2);   
-const outputText = " The sum of addition is " + result + "."; 
-
-//business logic
-function sub(number1, number2) {
-  return number1 - number2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-//user interface logic
-
-
-function saySomething(whatToSay) { 
-  window.alert(whatToSay);    
-}   
-
-const result2 = sub(number1, number2);  
-const outputText2 = " The remainder of subtraction is " + result2 + ".";   
-
-//business logic
-
-function mult(number1, number2) {
-  return number1 * number2
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-//user interface logic
-
-
-function saySomething(whatToSay) {   
-  window.alert(whatToSay);     
-}  
-
-const result3 = mult(number1, number2);   
-const outputText3 = " The result of multiplication is " + result3 + ".";   
-
-//business logic
-
-function div(number1, number2) {
-  return number1 / number2
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-//user interface logic
-
-
-
-function saySomething(whatToSay) {  
-  window.alert(whatToSay);  
-}  
-
-const result4 = div(number1, number2);
-const outputText4 = " The result of division is " + result4 + ".";
-
-//business logic
-
-function rem(number1, number2) {
-  return number1 % number2
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
 }
 
-//user interface logic
-
-
-function saySomething(whatToSay) {   
-  window.alert(whatToSay);    
-} 
-
-const result5 = rem(number1, number2); 
-const outputText5 = " The remainder after division is " + result5 + ".";
-
-saySomething(outputText + outputText2 + outputText3 + outputText4 + outputText5);
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
