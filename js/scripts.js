f7ee9dbafcd8b64cf8e8a2a4ac6342a1cc9d1aad
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-19 11:13:10 -0700

    update scripts.js to move branching to it's own function in the business logic section

--- js/scripts.js
+++ js/scripts.js
@@ -15,28 +15,31 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function calculate(num1, num2, operatorParam) {
+  if (operatorParam === "add") {
+    return add(num1, num2);
+  } else if (operatorParam === "subtract") {
+    return subtract(num1, num2);
+  } else if (operatorParam === "multiply") {
+    return multiply(num1, num2);
+  } else if (operatorParam === "divide") {
+    return divide(num1, num2);
+  }
+}
+
 // User Interface Logic
-function handleCalculation(event) {
+function handleSubmission(event) {
   event.preventDefault();
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
   const operator = document.querySelector("input[name='operator']:checked").value;
 
-  let result;
-  if (operator === "add") {
-    result = add(number1, number2);
-  } else if (operator === "subtract") {
-    result = subtract(number1, number2);
-  } else if (operator === "multiply") {
-    result = multiply(number1, number2);
-  } else if (operator === "divide") {
-    result = divide(number1, number2);
-  }
+  let result = calculate(number1, number2, operator);
 
   document.getElementById("output").innerText = result;
 }
 
 window.addEventListener("load", function() {
   const form = document.getElementById("calculator");
-  form.addEventListener("submit", handleCalculation);
+  form.addEventListener("submit", handleSubmission);
 });
